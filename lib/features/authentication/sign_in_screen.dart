f68243696bf50c18fc439ec00c1ca5979ff00992
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-26 19:49:08 +0900

    rendered sign_in_screen

--- lib/features/authentication/sign_in_screen.dart
+++ lib/features/authentication/sign_in_screen.dart
@@ -21,32 +21,153 @@ class _SignInScreenState extends State<SignInScreen> {
     return GestureDetector(
       onTap: _onScaffoldTap,
       child: Scaffold(
+        backgroundColor: const Color(0xFF1F2937),
         appBar: AppBar(
+          backgroundColor: const Color(0xFF1F2937),
           elevation: 0,
           title: const Text(
-            "MOOD",
+            "🔥 MOOD 🔥",
             style: TextStyle(
-              color: Colors.black,
+              color: Colors.white,
+              fontSize: 24,
               fontWeight: FontWeight.w600,
             ),
           ),
         ),
-        body: const Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: Sizes.size24,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size60,
           ),
           child: Column(
             children: [
               Gaps.v80,
-              Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
                     "Welcome!",
                     style: TextStyle(
-                      color: Colors.black,
-                      fontSize: Sizes.size20,
+                      color: Colors.white,
+                      fontSize: Sizes.size24,
                       fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+              Gaps.v44,
+              Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.grey.shade400,
+                      ),
+                      borderRadius: BorderRadius.circular(Sizes.size28),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: Sizes.size2,
+                        horizontal: Sizes.size16,
+                      ),
+                      child: TextField(
+                        // controller: () {},
+                        autocorrect: false,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          hintText: "Email",
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: Sizes.size18,
+                          ),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Gaps.v20,
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.grey.shade400,
+                      ),
+                      borderRadius: BorderRadius.circular(Sizes.size28),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: Sizes.size2,
+                        horizontal: Sizes.size16,
+                      ),
+                      child: TextField(
+                        // controller: _passwordController,
+                        autocorrect: false,
+                        keyboardType: TextInputType.visiblePassword,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: Sizes.size18,
+                          ),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Gaps.v28,
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      foregroundColor: const Color(0xFF0c64E0),
+                      backgroundColor: const Color(0xFF374151),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: Sizes.size10,
+                        horizontal: Sizes.size16,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(Sizes.size28),
+                        side: BorderSide(color: Colors.grey.shade400),
+                      ),
+                      minimumSize: const Size(double.infinity,
+                          0), // This will stretch the button horizontally
+                    ),
+                    child: const Text(
+                      "Log in",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 300,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      foregroundColor: const Color(0xFF0c64E0),
+                      backgroundColor: const Color(0xFF374151),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: Sizes.size10,
+                        horizontal: Sizes.size16,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(Sizes.size28),
+                        side: BorderSide(color: Colors.grey.shade400),
+                      ),
+                      minimumSize: const Size(double.infinity,
+                          0), // This will stretch the button horizontally
+                    ),
+                    child: const Text(
+                      "Create an account",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size18,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
                 ],
