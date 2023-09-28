c6e153b7995066e726abac9e77c19038730ea5b1
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-28 18:50:25 +0900

    removed focus on textbox when tapped on scaffold

--- lib/features/main/tab_screens/post_screen.dart
+++ lib/features/main/tab_screens/post_screen.dart
@@ -10,61 +10,71 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: Sizes.size20,
-        horizontal: Sizes.size32,
-      ),
-      child: Column(
-        children: [
-          Gaps.v40,
-          const Row(
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        backgroundColor: const Color(0xFF1F2937),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size20,
+            horizontal: Sizes.size32,
+          ),
+          child: Column(
             children: [
-              Text(
-                "How do you feel?",
-                style: TextStyle(
+              Gaps.v40,
+              const Row(
+                children: [
+                  Text(
+                    "How do you feel?",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: Sizes.size18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+              Gaps.v20,
+              Container(
+                decoration: BoxDecoration(
                   color: Colors.white,
-                  fontSize: Sizes.size18,
-                  fontWeight: FontWeight.w600,
+                  border: Border.all(
+                    color: Colors.grey.shade400,
+                  ),
+                  borderRadius: BorderRadius.circular(Sizes.size14),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: Sizes.size2,
+                    horizontal: Sizes.size16,
+                  ),
+                  child: TextField(
+                    // controller: (), // Uncomment this if you want to use a controller
+                    autocorrect: true,
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null, // Makes it expandable vertically
+                    minLines: 7, // Set this to your desired minimum lines
+                    decoration: InputDecoration(
+                      hintText: "Write it down here!",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontSize: Sizes.size16,
+                      ),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
                 ),
               ),
             ],
           ),
-          Gaps.v20,
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(
-                color: Colors.grey.shade400,
-              ),
-              borderRadius: BorderRadius.circular(Sizes.size14),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: Sizes.size2,
-                horizontal: Sizes.size16,
-              ),
-              child: TextField(
-                // controller: (), // Uncomment this if you want to use a controller
-                autocorrect: true,
-                keyboardType: TextInputType.multiline,
-                maxLines: null, // Makes it expandable vertically
-                minLines: 7, // Set this to your desired minimum lines
-                decoration: InputDecoration(
-                  hintText: "Write it down here!",
-                  hintStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: Sizes.size16,
-                  ),
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                ),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
