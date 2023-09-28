1c8e9c8e90f8e4599b1fcefb646b55269d9c2bf6
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-28 18:43:18 +0900

    rendered textbox on post_screen

--- lib/features/main/tab_screens/post_screen.dart
+++ lib/features/main/tab_screens/post_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_final_project/constants/gaps.dart';
 import 'package:flutter_final_project/constants/sizes.dart';
 
 class PostScreen extends StatefulWidget {
@@ -11,17 +12,56 @@ class PostScreen extends StatefulWidget {
 class _PostScreenState extends State<PostScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(
+    return Padding(
+      padding: const EdgeInsets.symmetric(
         vertical: Sizes.size20,
-        horizontal: Sizes.size20,
+        horizontal: Sizes.size32,
       ),
       child: Column(
         children: [
-          Text(
-            "Post",
-            style: TextStyle(
+          Gaps.v40,
+          const Row(
+            children: [
+              Text(
+                "How do you feel?",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: Sizes.size18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+          Gaps.v20,
+          Container(
+            decoration: BoxDecoration(
               color: Colors.white,
+              border: Border.all(
+                color: Colors.grey.shade400,
+              ),
+              borderRadius: BorderRadius.circular(Sizes.size14),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: Sizes.size2,
+                horizontal: Sizes.size16,
+              ),
+              child: TextField(
+                // controller: (), // Uncomment this if you want to use a controller
+                autocorrect: true,
+                keyboardType: TextInputType.multiline,
+                maxLines: null, // Makes it expandable vertically
+                minLines: 7, // Set this to your desired minimum lines
+                decoration: InputDecoration(
+                  hintText: "Write it down here!",
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: Sizes.size16,
+                  ),
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+              ),
             ),
           ),
         ],
